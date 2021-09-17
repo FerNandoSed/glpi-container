fa3383b1bde520f8f08051a23226f9d9424be505
FerNandoSed <f.j.cid.o@gmail.com>
2021-09-17 19:39:53 -0300

    Docker compose (#2)
    * Adds volume for document root, nginx is not longer a custom image and image of GLPI is depedent on GLPI version.
    
    * Avoids build custom image for nginx.
    
    * no longer needed.
    
    * Making the startup more verbose

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO:
+#   - find a way to upgrade by changing glpi image
+
 set -u
 
 FILE_DIRS="
@@ -21,6 +24,13 @@ _tmp
 LOCAL_DEFINE_CONTENT="<?php
 define('GLPI_VAR_DIR', '$GLPI_VAR_DIR');
 define('GLPI_LOG_DIR', '$GLPI_LOG_DIR');
+"
+DOWNSTREAM_CONTENT="<?php
+define('GLPI_CONFIG_DIR', '$GLPI_CONFIG_DIR');
+
+if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
+   require_once GLPI_CONFIG_DIR . '/local_define.php';
+}
 "
 
 ## From wait-for-it.sh ##
@@ -44,20 +54,41 @@ done
 
 set -e
 
+function copy_glpi {
+    # This function transfers GLPI to document root. Document root should be a persistent module.
+
+    # check if GLPI is already copied
+    if [ -f $GLPI_DOCUMENT_ROOT/index.php ]; then
+        echo "Not copying GLPI, since is already in document root"
+    else
+        echo "Copying GLPI (index.php is missing)"
+        tar -C $GLPI_DOCUMENT_ROOT/.. -xf /usr/local/src/glpi-9.5.6.tgz
+        echo "chowning $GLPI_DOCUMENT_ROOT to www-data..."
+        chown -R www-data:www-data $GLPI_DOCUMENT_ROOT
+    fi
+}
+
+# Check if GLPI is not installed. If installed, run php-fpm
 if [ -f $GLPI_CONFIG_DIR/config_db.php ]; then
-    [ -f $GLPI_DOCUMENT_ROOT/install/install.php ] && rm -v $GLPI_DOCUMENT_ROOT/install/install.php
+    echo "GLPI is installed..."
+    echo "starting php-fpm..."
     php-fpm
+
+# Install GLPI if not installed
 else
-    for DIR in $FILE_DIRS ; do
-        mkdir $GLPI_VAR_DIR/$DIR -p
-    done
 
-    chown -Rv www-data:www-data $GLPI_CONFIG_DIR $GLPI_LOG_DIR $GLPI_VAR_DIR
+    copy_glpi
 
-    cd $GLPI_DOCUMENT_ROOT    
+    # Create directories for GLPI files, since this is a custom position for GLPI variable data
+    if [ $GLPI_VAR_DIR != $GLPI_DOCUMENT_ROOT ]; then
+        for DIR in $FILE_DIRS ; do
+            mkdir $GLPI_VAR_DIR/$DIR -p
+        done
+    fi
 
-    echo $MYSQL_PASSWORD
+    cd $GLPI_DOCUMENT_ROOT
 
+    # This installs GLPI
     php bin/console db:install --db-host=$MYSQL_HOST \
         --db-name=$MYSQL_DATABASE \
         --db-user=$MYSQL_USER \
@@ -65,10 +96,15 @@ else
         -n \
         --force
     
+    # It is necessary to specify customs directories locations.
     echo $LOCAL_DEFINE_CONTENT > $GLPI_CONFIG_DIR/local_define.php
+    echo $DOWNSTREAM_CONTENT > $GLPI_DOCUMENT_ROOT/inc/downstream.php
 
-    chown -Rv www-data:www-data $GLPI_CONFIG_DIR $GLPI_LOG_DIR $GLPI_VAR_DIR
+    # GLPI should be run with www-data
+    echo "chowning custom directories to www-data..."
+    chown -R www-data:www-data $GLPI_CONFIG_DIR $GLPI_LOG_DIR $GLPI_VAR_DIR
 
+    # This is useful for security purposes and for determining if GLPI is already installed
     rm -v $GLPI_DOCUMENT_ROOT/install/install.php
 
     php-fpm
